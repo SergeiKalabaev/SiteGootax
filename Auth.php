3d0db7d4cb04ebed1b2ea28ab71ece50e2453ef6
unknown <sergei.kalabaev89@gmail.com>
2022-03-10 19:53:52 +0400

    deleted trash files, added login logic

--- Auth.php
+++ Auth.php
@@ -4,15 +4,49 @@
 <div class = "container-md">
     <div id = "login">
         <h1>Вход</h1>
-        <form action="loginform" method="post" name="loginform">
+        <form action="Auth.php" method="post" name="loginform">
         <p><label for="user_login">Имя пользователя<br>
-                <input class="input" id="username" name="username" size="20" type="text" value=""></label></p>
+                <input class="input" id="login" name="login" size="20" type="text" value=""></label></p>
         <p><label for="user_pass">Пароль<br>
                 <input class="input" id="password" name="password" size="20" type="password" value=""></label></p>
-            <p class="submit"><input class="button" name="login" type="submit" value="Log in"></p>
-            <p class="regtext">Еще не зарегестрированы? <a href ="registration.php">Регистрация</a></p>
+            <p class="submit"><input class="button" id="button" name="submit" type="submit" value="Log in"></p>
+            <p class="regtext">Еще не зарегистрированы? <a href ="registration.php">Регистрация</a></p>
         </form>
     </div>
 </div>
+
+<?php
+
+function generateCode($length=6)
+{
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
+    $code="";
+    $clen = strlen($chars) - 1;
+    while (strlen($code)<$length)
+    {
+        $code .= $chars[mt_rand(0,$clen)];
+    }
+    return $code;
+}
+$link = mysqli_connect("localhost","root","","testdb");
+    if (isset($_POST['submit']))
+    {
+        $query = mysqli_query($link, "SELECT user_id, user_password FROM users WHERE user_login ='".mysqli_real_escape_string($link,$_POST['login'])."' LIMIT 1");
+        $data = mysqli_fetch_assoc($query);
+
+        if($data['user_password']===md5(md5($_POST['password'])))
+        {
+            $hash = md5(generateCode(10));
+
+            setcookie("id",$data['user_id'],time()+60*60*24*30, "/");
+            header("Location: index.php");
+
+        }
+        else
+        {
+            print ("<b>Вы ввели неправильный логин или пароль</b><br>");
+        }
+    }
+?>
 <?php include ("includes/footer.php");?>
 
--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php include ("includes/header.php");?>
+<h1>Hello,World</h1>
 <?php include ("includes/footer.php");?>
